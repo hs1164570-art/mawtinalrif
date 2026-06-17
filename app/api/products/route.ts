b45673c677479dc112baf7a6da41772b798ef529
hs1164570-art <hs1164570@gmail.com>
2026-06-17 18:39:03 +0300

    fix: final middleware fix for vercel edge

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getProductsData } from "@/utils/getProductsBySlug";
-export const runtime = "nodejs";
 
 export async function GET(request: NextRequest) {
   try {
